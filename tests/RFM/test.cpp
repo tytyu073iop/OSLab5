0eb0de0ab712c776dfb11e8b756f8672f0aa2e27
tytyu073iop <fpm.biryuk@bsu.by>
2025-05-17 12:27:52 +0300

    added second multiple test

--- tests/RFM/test.cpp
+++ tests/RFM/test.cpp
@@ -14,3 +14,21 @@ TEST(TestRFM, General) {
     rfm.edit(1, g);
     EXPECT_EQ(g, rfm.read(1));
 }
+
+TEST(TestRFM, GeneralMultiple) {
+    employee e{.num = 1, .name = "Ilya", .hours = 5};
+    employee g{.num = 2, .name = "Ilya", .hours = 6};
+    std::ifstream in("test");
+    std::ofstream out("test");
+    RFM rfm(in, out);
+
+    rfm.add(e);
+    rfm.add(g);
+    employee ec = rfm.read(1);
+    employee gc = rfm.read(2);
+    EXPECT_EQ(e, ec);
+    EXPECT_EQ(g, gc);
+    employee gq{.num = 1, .name = "Ilya2", .hours = 6};
+    rfm.edit(1, gq);
+    EXPECT_EQ(gq, rfm.read(1));
+}
